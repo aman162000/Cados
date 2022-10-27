4025f4ca206363be7caa905241d975e005533255
aman162000 <borseaman16@gmail.com>
2022-10-27 20:27:01 +0530

    Update settings.py

--- advocate/settings.py
+++ advocate/settings.py
@@ -170,7 +170,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 CORS_ALLOW_ALL_ORIGINS = True
 
